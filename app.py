94cb09f8a098b8cb0fa0b0f85780d5386a94c4db
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-24 22:58:57 -0400

    wrote login function

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-from connection import db
+from db.connection import db
+from db.models import User, Note
+
+login_prompt = input('Do you want to login or create a user?\n')
+
+if login_prompt == 'login':
+    login()
+else:
+    create_user()
+
+def login():
+    username = input('Enter username:')
+    user = User.get(User.username == username)
+    print(user)
