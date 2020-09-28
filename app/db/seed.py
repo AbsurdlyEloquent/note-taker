124035ca6c1c0e2eb653c4e4d2a69df58640eb7b
Ethan Henderson <ethan.henderson397@gmail.com>
2020-09-27 20:45:02 -0400

    wrote seed

--- app/db/seed.py
+++ app/db/seed.py
@@ -1,6 +1,44 @@
 import models
 from connection import db
+from datetime import datetime
 
 db.connect()
 db.drop_tables([models.Notes])
 db.create_tables([models.Notes])
+
+notes = [{
+    'title': "New Note",
+    'contents': 'This is an important message',
+    'timestamp': datetime.now(),
+    'username': 'psql'
+},{
+    'title': "rm reminder",
+    'contents': 'DON\'T USE rm -rf EVER',
+    'timestamp': datetime.now(),
+    'username': 'Ethan'
+},{
+    'title': 'How does css work?',
+    'contents': 'box-sizing: border-box;',
+    'timestamp': datetime.now(),
+    'username': 'psql'
+},{
+    'title': "Grocery List",
+    'contents': 'Eggs, Milk, Bread, Yogurt',
+    'timestamp': datetime.now(),
+    'username': 'Ethan'
+},{
+    'title': "Databases are rad",
+    'contents': '😀😃😄😁😆😅😂🤣☺️😇🙂🙃😉😍😘😗😙😋😛😝😜🤪🤨🧐🤓😎🤩😏😒😞😔😟😕🙁☹️😣😖',
+    'timestamp': datetime.now(),
+    'username': 'psql'
+},{
+    'title': "...",
+    'contents': 'uhhh, running out of ideas',
+    'timestamp': datetime.now(),
+    'username': 'Ethan'
+},{
+    'title': "Make a README!",
+    'contents': 'Don\'t forget to make your README.md! It\'s this most important part!',
+    'timestamp': datetime.now(),
+    'username': 'psql'
+}]
